499dd4c2e37d60255059eb42700338a1d1d2be28
Muhammad Ubaid ur Raheem <shahbazharoon@hotmail.com>
2025-11-14 02:24:00 +0300

    Add Lombok @Setter annotation to UserRequestDTO

--- src/main/java/com/ubaidsample/h2/dto/request/UserRequestDTO.java
+++ src/main/java/com/ubaidsample/h2/dto/request/UserRequestDTO.java
@@ -13,9 +13,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRequestDTO {
